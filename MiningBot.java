18c1db3715479953ac9d5fe6d9975a5a071a4e13
houkevin <43103422+houkevin@users.noreply.github.com>
2018-09-20 20:15:55 -0400

    Update MiningBot.java

--- MiningBot.java
+++ MiningBot.java
@@ -5,12 +5,12 @@ import org.powerbot.script.Script;
 import org.powerbot.script.rt4.ClientContext;
 import java.util.*;
 
-@Script.Manifest(name="ChopScript",description="This chops stuff",properties="client=4; topic =123456789; author=Kevin;")
-public class ChopScript extends PollingScript<ClientContext>{
+@Script.Manifest(name="MiningScript",description="This mines stuff",properties="client=4; topic =123456789; author=Kevin;")
+public class MineScript extends PollingScript<ClientContext>{
     private List<Task> taskList = new ArrayList<Task>();
     @Override
     public void start(){
-        taskList.addAll(Arrays.asList(new Chop(ctx), new Drop(ctx)));
+        taskList.addAll(Arrays.asList(new Mine(ctx), new Drop(ctx)));
     }
     
     @Override
